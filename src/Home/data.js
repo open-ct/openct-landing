188393624179fe0dfd511263d139f583dfa130e2
Haifeng Luo <lhf@mail.bnu.edu.cn>
2019-12-25 19:27:33 +0800

    Add another row.

--- src/Home/data.js
+++ src/Home/data.js
@@ -107,6 +107,30 @@ export const page1 = {
       shadowColor: 'rgba(112, 73, 166, 0.08)',
       link: 'https://antv.alipay.com/zh-cn/vis/index.html',
     },
+    {
+      title: '设计价值观',
+      content: 'Design Values',
+      src: 'https://gw.alipayobjects.com/zos/rmsportal/KtRzkMmxBuWCVjPbBgRY.svg',
+      color: '#EB2F96',
+      shadowColor: 'rgba(166, 55, 112, 0.08)',
+      link: 'https://ant.design/docs/spec/values-cn',
+    },
+    {
+      title: '视觉',
+      content: 'Visual',
+      src: 'https://gw.alipayobjects.com/zos/rmsportal/qIcZMXoztWjrnxzCNTHv.svg',
+      color: '#1890FF',
+      shadowColor: 'rgba(15, 93, 166, 0.08)',
+      link: 'https://ant.design/docs/spec/colors-cn',
+    },
+    {
+      title: '可视化',
+      content: 'Visualisation',
+      src: 'https://gw.alipayobjects.com/zos/rmsportal/eLtHtrKjXfabZfRchvVT.svg',
+      color: '#AB33F7',
+      shadowColor: 'rgba(112, 73, 166, 0.08)',
+      link: 'https://antv.alipay.com/zh-cn/vis/index.html',
+    },
   ],
 };
 
